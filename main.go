c8a67a2cd08a5c8221f32343d2844a5936e6b3b1
fumi <expugnatiomundi@gmail.com>
2025-10-13 18:20:23 +0900

    feat: implement repel

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func printPrompt() {
+	fmt.Print("db > ")
+}
+
+func readInput(reader *bufio.Reader) string {
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		os.Exit(1)
+	}
+	return strings.TrimSpace(input)
+}
 
 func main() {
-	fmt.Println("hello")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		printPrompt()
+		input := readInput(reader)
+		if input == ".exit" {
+			os.Exit(0)
+		} else {
+			fmt.Printf("unrecognized command: %s\n", input)
+		}
+	}
 }
